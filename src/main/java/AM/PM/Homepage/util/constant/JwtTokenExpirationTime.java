b2ddd2cdaab7f612f73f1cf59168d67a7c631211
notYourOcean <wjswjdgus333@gmail.com>
2025-06-26 22:24:56 +0900

    feat: 상수클래스 생성 제한

--- src/main/java/AM/PM/Homepage/util/constant/JwtTokenExpirationTime.java
+++ src/main/java/AM/PM/Homepage/util/constant/JwtTokenExpirationTime.java
@@ -5,4 +5,7 @@ public final class JwtTokenExpirationTime {
     public final static long accessExpirationMinutes = 60 * 30 * 1000;
     public final static long refreshExpirationHours = 7 * 24 * 60 * 30 * 1000;
 
+    private JwtTokenExpirationTime() {
+    }
+
 }
