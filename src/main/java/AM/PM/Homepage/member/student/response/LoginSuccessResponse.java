c8cf96e782ebb5a0e06ee4f6bc43f2676837d4ed
notYourOcean <wjswjdgus333@gmail.com>
2025-09-11 17:41:03 +0900

    feat: 로그인 성공시 응답값을 위한 handler 추가

---
+++ src/main/java/AM/PM/Homepage/member/student/response/LoginSuccessResponse.java
@@ -0,0 +1,18 @@
+package AM.PM.Homepage.member.student.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginSuccessResponse {
+
+    private String studentNumber;
+    private Long studentId;
+    private String studentTier;
+
+}
