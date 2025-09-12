170b0f0032b0e96c7a5040575e6c77c0e5042477
notYourOcean <wjswjdgus333@gmail.com>
2025-09-12 18:49:15 +0900

    feat: 크롤링 기능 구축 및 엔티티 추가

---
+++ src/main/java/AM/PM/Homepage/cafeteria/domain/CafeteriaMenu.java
@@ -0,0 +1,43 @@
+package AM.PM.Homepage.cafeteria.domain;
+
+import AM.PM.Homepage.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CafeteriaMenu extends BaseEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cafeteria_id")
+    private Long id;
+
+    @Column(name = "cafeteria_name")
+    private String cafeteria;
+
+    @Column(name = "meal_date")
+    private LocalDate mealDate;
+
+    @Column(name = "meal_price")
+    private Integer mealPrice;
+
+    @Column(name = "meal_name")
+    private String mealName;
+
+    @Column(name = "meal_type")
+    private String mealType;
+
+
+
+
+}
