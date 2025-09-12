e8792d046fa3f4475bcbee72f4165a2f26c7cf7a
notYourOcean <wjswjdgus333@gmail.com>
2025-09-12 19:33:56 +0900

    feat: controller 추가

---
+++ src/main/java/AM/PM/Homepage/cafeteria/controller/CafeteriaController.java
@@ -0,0 +1,7 @@
+package AM.PM.Homepage.cafeteria.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/cafeteria")
+public class CafeteriaController {
+}
