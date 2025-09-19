d7db088e25884fe62606521fbeccffca8dab1833
notYourOcean <wjswjdgus333@gmail.com>
2025-09-20 04:20:58 +0900

    refactor: info api 수정

--- src/main/java/AM/PM/Homepage/member/student/response/VerificationCodeResponse.java
+++ src/main/java/AM/PM/Homepage/member/student/response/VerificationCodeResponse.java
@@ -1,11 +1,11 @@
 package AM.PM.Homepage.member.student.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class VerificationCodeResponse {
 
     private String solvedAcNickname;
