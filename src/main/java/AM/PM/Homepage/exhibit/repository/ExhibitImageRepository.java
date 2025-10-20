e4e4beab2757b5aae1418316633e220aa926b028
박병훈 <leafloveu@naver.com>
2025-10-20 23:15:08 +0900

    fix(exhibit): 전시 게시글 이미지 엔티티 삭제
    - exhibitImage entity, repo 삭제 및 exhibit 연관관계 삭제
    - service, dto, controller에서 관련 부분 삭제
    - ExhibitAuthorizationChecker 추가

--- src/main/java/AM/PM/Homepage/exhibit/repository/ExhibitImageRepository.java
+++
@@ -1,7 +0,0 @@
-package AM.PM.Homepage.exhibit.repository;
-
-import AM.PM.Homepage.exhibit.entity.ExhibitImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExhibitImageRepository extends JpaRepository<ExhibitImage, Long> {
-}
--- src/main/java/AM/PM/Homepage/member/student/domain/Student.java
+++ src/main/java/AM/PM/Homepage/member/student/domain/Student.java
@@ -20,8 +20,6 @@ import jakarta.persistence.Table;
 import java.util.List;
 import java.util.UUID;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
